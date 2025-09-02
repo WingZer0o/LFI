ee89215783a15719bdf99690d2803e34d6ebc15e
WingZer0o <mikemulchrone987@gmail.com>
2025-09-01 22:51:34 -0400

    init

---
+++ Startup/Main.cs
@@ -0,0 +1,15 @@
+namespace LFI {
+    public static class MainStartup
+    {
+
+        public static void DisplayHeader()
+        {
+            Console.WriteLine("******LFI Scanner***********");
+        }
+
+        public static void DisplayFooter()
+        {
+            Console.WriteLine("****************************");
+        }
+    }
+}
